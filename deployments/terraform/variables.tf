1ab3546bf8a13af6fb36d530f9c94949cb96325a
None <maksiess@bastion-fuchicorp-com.us-central1-a.c.geometric-sled-282503.internal>
2020-12-23 04:39:36 +0000

    added and fixed

--- deployments/terraform/variables.tf
+++ deployments/terraform/variables.tf
@@ -1,29 +1,19 @@
+variable  "deployment_image" {
+    default = "docker.fuchicorp.com/hello-world-app-dev-feature:4b7aacb"
+}
 variable "deployment_environment" {
     default = "dev"
-    description = "- (Optional) The namespace to deploy the helm chart."
-
 }
-
-variable "deployment_name" {
-    default = "hello-world"
-    description = "- (Optional) The name of the deployment"
-
-}
-variable "deployment_image" {
-    default = "fsadykov/hello-world:latest"
-    description = "- (Optional) The docker image of the deployment"
-
-}
-
 variable "deployment_endpoint" {
-    type - "map"
-
-    default = {
-        dev = "dev.hello.maxirs.com"
-        qa = "qa.hello.maxirs.com"
-        prod = "prod.hello.maxirs.com"
-        stage = "stage.hello.maxirs.com"
-    }
-    description = "- (Optional) The endpoint of the deployment"
-
+    type = "map"
+     default = {
+        test  = "test.hello"
+        dev  = "dev.hello"
+        qa   = "qa.hello"
+        prod = "hello"
+        stage = "stage.hello"
+  }
+}
+variable "google_domain_name" {
+    default = "fuchicorp.com"
 }
